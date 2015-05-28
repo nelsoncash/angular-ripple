75eb788a7b41b831a76f30e2185b2dbf123c8c7c
Michael McMillan <mikemcchillin@gmail.com>
2015-05-28 14:01:45 -0500

    Updated demo

--- angular-ripple.js
+++ angular-ripple.js
@@ -1,5 +1,5 @@
 /*!
- * angular-ripple.js v0.0.3 - A standalone AngularJS implementation of the Google Material Design ripple effect.
+ * angular-ripple.js v0.0.4 - A standalone AngularJS implementation of the Google Material Design ripple effect.
  * Copyright (c) 2014 Nelson Cash - http://github.com/nelsoncash/angular-ripple
  * http://codepen.io/MikeMcChillin/pen/XJrLwg
  * License: MIT
@@ -18,56 +18,62 @@
     return {
       restrict: 'A',
       link: function (scope, element, attrs) {
-        var x, y, size, offsets;
-
-        element.on('click touchstart', function(e) {
-          var ripple = this.querySelector('.angular-ripple');
-          var eventType = e.type;
-          // Ripple
-          if (ripple === null) {
-            // Create ripple
-            ripple = document.createElement('span');
-            ripple.classList.add('angular-ripple');
-
-            // Prepend ripple to element
-            this.insertBefore(ripple, this.firstChild);
-
-            // Set ripple size
-            if (!ripple.offsetHeight && !ripple.offsetWidth) {
-              size = Math.max(element[0].offsetWidth, element[0].offsetHeight);
-              ripple.style.width = size + 'px';
-              ripple.style.height = size + 'px';
+        var x, y, size, offsets,
+          func = function(e){
+            var ripple = this.querySelector('.angular-ripple');
+            var eventType = e.type;
+            // Ripple
+            if (ripple === null) {
+              // Create ripple
+              ripple = document.createElement('span');
+              ripple.classList.add('angular-ripple');
+  
+              // Prepend ripple to element
+              this.insertBefore(ripple, this.firstChild);
+  
+              // Set ripple size
+              if (!ripple.offsetHeight && !ripple.offsetWidth) {
+                size = Math.max(element[0].offsetWidth, element[0].offsetHeight);
+                ripple.style.width = size + 'px';
+                ripple.style.height = size + 'px';
+              }
             }
+  
+            // Remove animation effect
+            ripple.classList.remove('animate');
+  
+            // get click coordinates by event type
+            if (eventType === 'click') {
+              x = e.pageX;
+              y = e.pageY;
+            } else if(eventType === 'touchstart') {
+              x = e.changedTouches[0].pageX;
+              y = e.changedTouches[0].pageY;
+            }
+  
+            // set new ripple position by click or touch position
+            function getPos(element) {
+              var de = document.documentElement;
+              var box = element.getBoundingClientRect();
+              var top = box.top + window.pageYOffset - de.clientTop;
+              var left = box.left + window.pageXOffset - de.clientLeft;
+              return { top: top, left: left };
+            }
+  
+            offsets = getPos(element[0]);
+            ripple.style.left = (x - offsets.left - size / 2) + 'px';
+            ripple.style.top = (y - offsets.top - size / 2) + 'px';
+  
+            // Add animation effect
+            ripple.classList.add('animate');          
+            
           }
 
-          // Remove animation effect
-          ripple.classList.remove('animate');
-
-          // get click coordinates by event type
-          if (eventType === 'click') {
-            x = e.pageX;
-            y = e.pageY;
-          } else if(eventType === 'touchstart') {
-            x = e.changedTouches[0].pageX;
-            y = e.changedTouches[0].pageY;
-          }
-
-          // set new ripple position by click or touch position
-          function getPos(element) {
-            var de = document.documentElement;
-            var box = element.getBoundingClientRect();
-            var top = box.top + window.pageYOffset - de.clientTop;
-            var left = box.left + window.pageXOffset - de.clientLeft;
-            return { top: top, left: left };
-          }
-
-          offsets = getPos(element[0]);
-          ripple.style.left = (x - offsets.left - size / 2) + 'px';
-          ripple.style.top = (y - offsets.top - size / 2) + 'px';
-
-          // Add animation effect
-          ripple.classList.add('animate');
-
+        element.on('click touchstart', func);
+    
+        //remove the event listener on scope destroy
+        scope.$on('$destroy',function() {
+          element.off('click touchstart', func);
         });
       }
     };
