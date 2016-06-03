485a0df9151b8f3b6db559d39fb2141e5e1d793c
Michael McMillan <mikemcchillin@gmail.com>
2016-06-03 14:40:00 -0500

    Incremented version num

--- angular-ripple.js
+++ angular-ripple.js
@@ -1,5 +1,5 @@
 /*!
- * angular-ripple.js v0.0.5 - A standalone AngularJS implementation of the Google Material Design ripple effect.
+ * angular-ripple.js v0.0.6 - A standalone AngularJS implementation of the Google Material Design ripple effect.
  * Copyright (c) 2014 Nelson Cash - http://github.com/nelsoncash/angular-ripple
  * http://codepen.io/MikeMcChillin/pen/XJrLwg
  * License: MIT
